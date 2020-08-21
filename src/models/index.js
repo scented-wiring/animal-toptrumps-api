fb8cad18a8ddf9a65adc8d05cf718aff55dd2cb6
Tom Hammersley <thomas.j.hammersley@gmail.com>
2020-08-21 17:55:40 +0100

    set up project

---
+++ src/models/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const setupDatabase () => {
+  const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+      host: DB_HOST,
+      port: DB_PORT,
+  })
+
+  connection.sync({alter:true});
+  return {};
+};
+
+module.exports = setupDatabase();
